8b8ba6854ecd53980d37f675d009be1a34a4921e
ronsun <ron.sun@hotmail.com.tw>
2020-02-12 01:57:59 +0800

    init + create MVC website "Client.MVC" that dependent to class library "Library" + Both project dependent to package "Newtonsoft.Json" from NuGet + Update "Newtonsoft.Json" to older version 6.0.1 for further demo + Call `Worker.Do()` on Global.asax of project "Client.MVC" and make sure "Client.MVC" can be launched and works well.

---
+++ Client.MVC/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Client.MVC")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Client.MVC")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("14091f37-593c-4499-9ab6-0b53fa7cc0bf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Library/Worker.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+namespace Library
+{
+    public class Worker
+    {
+        public static void Do()
+        {
+            Debug.WriteLine($"{nameof(Worker)}.{nameof(Do)}() called.");
+        }
+    }
+}
